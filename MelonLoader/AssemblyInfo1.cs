c23ba03f0bf0eba72aa8bac0c60d98decc93c43a
Hilmy Mulyana <hmul0003@student.monash.edu>
2024-06-27 00:45:30 +1000

    melonloader mod for dtr and source

---
+++ MelonLoader/AssemblyInfo1.cs
@@ -0,0 +1,5 @@
+﻿using MelonLoader;
+using MyMod; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(SerialPatch), "DtrEnable", "0.1", "rumi")]
+[assembly: MelonGame("", "")]
---
+++ MelonLoader/Class1.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using IO;
+using System.IO.Ports;
+using System.Reflection;
+
+namespace MyMod
+{
+    [HarmonyPatch(typeof(NewTouchPanel), "Open")]
+    public class SerialPatch : MelonMod
+    {
+        static bool Prefix(uint ____monitorIndex, string[] ___PortName, ref SerialPort ____serialPort, int ___BaudRate)
+        {
+            {
+                try
+                {
+                    ____serialPort = new SerialPort(___PortName[____monitorIndex], ___BaudRate, Parity.None, 8, StopBits.One);
+                    ____serialPort.ReadTimeout = 1;
+                    ____serialPort.WriteTimeout = 1000;
+                    ____serialPort.DtrEnable = true;
+                    ____serialPort.Open();
+                }
+                catch
+                {
+                }
+            }
+            return false;
+        }
+        public override void OnInitializeMelon()
+        {
+            //FieldInfo serialPort = typeof(NewTouchPanel).GetField("_serialPort", BindingFlags.NonPublic | BindingFlags.Instance);
+            //object[] dtrEnabled = serialPort.GetCustomAttributes(typeof(bool), false);
+            LoggerInstance.Msg($"DTR Enabled");
+        }
+    }
+}
